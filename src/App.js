d2ae45dab9972c379cad7874b4200864e9beb9c7
Cameron Blackwood <cameronjwblackwood@gmail.com>
2019-11-21 09:53:37 +0000

    added image and title

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
+import shine from './energy.svg';
 
 function App() {
   return (
     <div className="App">
-        Shine Energy
+        <h1>Shine Energy</h1>
+        
+        <img src={shine} alt="our logo"/>
     </div>
   );
 }
