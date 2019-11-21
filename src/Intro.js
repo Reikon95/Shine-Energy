6b5766b7dcecc2c8fc03b1ce6cf53944af4b51ca
Cameron Blackwood <cameronjwblackwood@gmail.com>
2019-11-21 10:52:24 +0000

    fixed refresh

--- src/Intro.js
+++ src/Intro.js
@@ -2,11 +2,11 @@ import React, { useState, useEffect } from 'react';
 import Calculator from './Calc.js'
 import shine from './energy.svg';
 function Intro() {
-    let [clicked, setClicked] = useState(false);
+  let [clicked, setClicked] = useState(false)
     function handleClick() {
-        setClicked(true);
-    }
-    return !clicked? (
+      setClicked(true);
+    } 
+    return clicked === false ? (
       <div className="Intro">
           <h1>Shine Energy</h1>
   
@@ -15,7 +15,7 @@ function Intro() {
           <input type="text"></input>
           Please enter your electricity reading:
           <input type="text"></input>
-          <button onClick={handleClick()}>Calculate my Bill</button>
+          <button onClick={handleClick}>Calculate my Bill</button>
       </div>
     ) :  (
             <Calculator/>
