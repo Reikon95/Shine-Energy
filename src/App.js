930b24793d651c4ba89995e4e590919ee2153774
Zara <48896844+zarbar@users.noreply.github.com>
2019-11-21 12:49:29 +0000

    cam stuff added

--- src/App.js
+++ src/App.js
@@ -4,15 +4,12 @@ import Logo from './Logo/Logo';
 import Welcome from './Welcome/Welcome';
 import Form from './Form/Form';
 
-import Calculator from './Calc.js'
-
 function App() {
   return (
     <div className="App">
       <Logo />
       <Welcome userName={'Graham'} />
       <Form />
-
     </div>
   );
 }
